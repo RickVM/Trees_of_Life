18698c891b2f109273f295eda56334bf377b29e8
Rick van Melis <vanmelisrick@gmail.com>
2017-10-10 19:01:29 +0200

    First commit

---
+++ Animator/glow/Communication.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <Arduino.h>
+
+enum COMMANDS{
+  error,
+  pulse,
+  flash,
+  backward
+  };
+
+class Communication
+{
+  private:
+    int baudRate;
+    int port;
+    int readByte;
+    char receivedChar;
+    bool command;
+    String msg;
+ 
+    String _format(String message);//Function tested
+    COMMANDS checkProtocol();
+
+  public:
+    Communication(uint8_t port, uint32_t baudrate);
+    void Begin();
+    COMMANDS readSerialPort();
+    uint8_t sendMessage(String message);
+};
+
---
+++ ir_test/ir_test.ino
@@ -0,0 +1,74 @@
+#include <DistanceGP2Y0A21YK.h>
+
+#define trigPin 2
+#define echoPin 3
+#define led 13
+
+DistanceGP2Y0A21YK Dist;
+int distance;
+
+
+
+#define sensorPin A7
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Dist.begin(sensorPin);
+
+    pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+}
+  
+void loop() {
+  // put your main code here, to run repeatedly:
+  //printIr();
+  printUs();
+  delay(500);
+}
+
+void printIr(){
+distance = Dist.getDistanceCentimeter();
+  double val = 0;
+  int i = analogRead(sensorPin);
+  val = (6762 / (i - 9)) - 4;
+
+  Serial.print("\nDistance in: ");
+  Serial.print("\t Voltage:");
+  Serial.print(i);
+  Serial.print("\t formula: ");
+  Serial.print(val);
+  Serial.print("\t library: ");
+  Serial.println(distance);
+}
+
+
+void printUs(){
+    long duration, distance;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(1000); - Removed this line
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  if (distance < 4) {  // This is where the LED On/Off happens
+    digitalWrite(led,HIGH); // When the Red condition is met, the Green LED should turn off
+  
+}
+  else {
+    digitalWrite(led,LOW);
+    
+  }
+  if (distance >= 200 || distance <= 0){
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print("Distance from us: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  //Serial.println(duration);
+}
