0f4f0afddfb648abe70411191f6f01c936495216
Unknown <simon.lit@live.nl>
2017-10-19 15:04:23 +0200

    Ultrasoon werkend, button debounce, logica
    Toegevoegd een sync delay

--- Controller/Controller_v3.0/Communication.cpp
+++ Controller/Controller_v3.0/Communication.cpp
@@ -11,9 +11,17 @@ String Communication::_format(String message)
 COMMANDS Communication::checkProtocol(String message)
 {
   COMMANDS rv = error;
-  if(message == "pulse")
+  if(message == "pulse1")
   {
-    rv = pulse;
+    rv = pulse1;
+  }
+  else if(message == "pulse2")
+  {
+    rv = pulse2;
+  }
+  else if(message == "pulse3")
+  {
+    rv = pulse3;
   }
   else if(message == "flash")
   {
